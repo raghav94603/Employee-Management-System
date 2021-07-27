84a82fd65802ce51cdcb844f2db0c4d21516bb34
Sarthak Naithani <sarthaknaithani@yahoo.in>
2021-07-27 19:03:28 +0530

    Integrated with the invalid page jsp.

---
+++ Employee-Management-System/src/main/java/com/ltts/ems/controller/LoginAuthentication.java
@@ -0,0 +1,39 @@
+package com.ltts.ems.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ltts.ems.repository.EmployeeDAO;
+
+@RestController
+public class LoginAuthentication{
+	@Autowired
+	EmployeeDAO emp;
+	@RequestMapping("/")
+	public String Login() {
+		return ("login");
+	}
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ModelAndView LoginAuth(HttpServletRequest request)
+	{
+		String username=request.getParameter("usrname");
+		String password=request.getParameter("psw");
+		String uname=emp.getUserbyusername(username, password);
+		if(uname==null) {
+			//System.out.print("Error");
+			return new ModelAndView("Invalid_user");
+		}
+		else 
+		{
+			//System.out.print("Success");
+			return new ModelAndView("#");
+		}
+	
+	}
+
+}
