addf3413c6a659982530153fbbd2e17e9d79d2a7
Sarthak Naithani <sarthaknaithani@yahoo.in>
2021-07-30 17:16:31 +0530

    Added add user functionality

--- Employee-Management-System/src/main/java/com/ltts/ems/controller/welcomeController.java
+++ Employee-Management-System/src/main/java/com/ltts/ems/controller/welcomeController.java
@@ -1,5 +1,20 @@
 package com.ltts.ems.controller;
 
-public class welcomeController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ltts.ems.repository.EmployeeDAO;
+
+@RestController
+public class welcomeController {
+	@Autowired
+	EmployeeDAO emp;
+@PostMapping("/add")
+public String addUser(HttpServletRequest request) {
+	emp.addUser(request.getParameter("id"),request.getParameter("doJ"), request.getParameter("doB"), request.getParameter("employeeNumber"), request.getParameter("firstName"), request.getParameter("gender"), request.getParameter("img"), request.getParameter("lastName"), request.getParameter("role"), request.getParameter("username"), request.getParameter("password"));
+	return "Updated";
+}
 }
--- Employee-Management-System/src/main/java/com/ltts/ems/repository/EmployeeDAO.java
+++ Employee-Management-System/src/main/java/com/ltts/ems/repository/EmployeeDAO.java
@@ -1,15 +1,21 @@
 package com.ltts.ems.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ltts.ems.model.Employeedetails;
-
+@Repository
 public interface EmployeeDAO extends JpaRepository<Employeedetails,Integer> {
 
 @Query("Select username from Employeedetails e where e.username= :username and e.password= :password")
 public String getUserbyusername(@Param("username") String username,@Param("password") String password);
+@Modifying
+@Query(value= "Insert into employeedetails values(:id,:doJ, :dob, :employeeNumber, :firstName, :gender, :img, :lastName ,:role,:username, :password)",nativeQuery = true)
+@Transactional
+public int addUser(@Param("id") String id,@Param("doJ") String doJ,@Param("dob") String dob, @Param("employeeNumber") String employeeNumber,@Param("firstName") String firstName, @Param("gender") String gender,@Param("img") String img,@Param("lastName") String lastName,@Param("role") String role,@Param("username") String username,@Param("password") String password);
 
 }
