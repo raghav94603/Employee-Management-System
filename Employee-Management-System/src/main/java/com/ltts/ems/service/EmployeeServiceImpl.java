c704e24e098f4ffa405f28252a88f38698aae20a
Sarthak Naithani <sarthaknaithani@yahoo.in>
2021-08-02 12:29:22 +0530

    Added add user and delete user controller

--- Employee-Management-System/src/main/java/com/ltts/ems/service/EmployeeServiceImpl.java
+++ Employee-Management-System/src/main/java/com/ltts/ems/service/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ltts.ems.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return emp.deleteById(id);
 		
 	}
+	@Override
+	public List<Employeedetails> findAll() {
+		
+		return emp.findAll();
+	}
+
 	
 	
 
